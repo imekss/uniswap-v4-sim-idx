b1b7baa37e351dbc5fcd9c69ab7c3ad90727c3fd
imekss <dgn.ousmane@gmail.com>
2025-11-22 12:28:41 -0300

    Adding chains

--- listeners/src/Main.sol
+++ listeners/src/Main.sol
@@ -11,20 +11,72 @@ contract Triggers is BaseTriggers {
     function triggers() external virtual override {
         PoolManagerListener listener = new PoolManagerListener();
 
-        // Ethereum
+        // ETHEREUMM
         addTrigger(chainContract(Chains.Ethereum, POOLMANAGER_ETHEREUM),listener.triggerOnInitializeEvent());
         addTrigger(chainContract(Chains.Ethereum, POOLMANAGER_ETHEREUM),listener.triggerOnSwapEvent());
-        // addTrigger(chainContract(Chains.Ethereum, POOLMANAGER_ETHEREUM),listener.triggerOnModifyLiquidityEvent());
 
-        // Unichain
+        // UNICHAIN
         addTrigger(chainContract(Chains.Unichain, POOLMANAGER_UNICHAIN),listener.triggerOnInitializeEvent());
         addTrigger(chainContract(Chains.Unichain, POOLMANAGER_UNICHAIN),listener.triggerOnSwapEvent());
-        // addTrigger(chainContract(Chains.Ethereum, POOLMANAGER_UNICHAIN),listener.triggerOnModifyLiquidityEvent());
 
-        // Base
+
+        // OPTIMISM
+        addTrigger(chainContract(Chains.Optimism, POOLMANAGER_OPTIMISM),listener.triggerOnInitializeEvent());
+        addTrigger(chainContract(Chains.Optimism, POOLMANAGER_OPTIMISM),listener.triggerOnSwapEvent());
+
+
+        // BASE
         addTrigger(chainContract(Chains.Base, POOLMANAGER_BASE),listener.triggerOnInitializeEvent());
         addTrigger(chainContract(Chains.Base, POOLMANAGER_BASE),listener.triggerOnSwapEvent());
-        // addTrigger(chainContract(Chains.Ethereum, POOLMANAGER_BASE),
 
+
+        // ARBITRUM
+        addTrigger(chainContract(Chains.Arbitrum, POOLMANAGER_ARBITRUM),listener.triggerOnInitializeEvent());
+        addTrigger(chainContract(Chains.Arbitrum, POOLMANAGER_ARBITRUM),listener.triggerOnSwapEvent());
+
+
+        // POLYGON
+        addTrigger(chainContract(Chains.Polygon, POOLMANAGER_POLYGON),listener.triggerOnInitializeEvent());
+        addTrigger(chainContract(Chains.Polygon, POOLMANAGER_POLYGON),listener.triggerOnSwapEvent());   
+
+
+        // BLAST
+        addTrigger(chainContract(Chains.Blast, POOLMANAGER_BLAST),listener.triggerOnInitializeEvent());
+        addTrigger(chainContract(Chains.Blast, POOLMANAGER_BLAST),listener.triggerOnSwapEvent());   
+
+
+        // ZORA 
+        addTrigger(chainContract(Chains.Zora, POOLMANAGER_ZORA),listener.triggerOnInitializeEvent());
+        addTrigger(chainContract(Chains.Zora, POOLMANAGER_ZORA),listener.triggerOnSwapEvent());
+
+
+        // WORLDCOIN
+        addTrigger(chainContract(Chains.Worldcoin, POOLMANAGER_WORLDCOIN),listener.triggerOnInitializeEvent());
+        addTrigger(chainContract(Chains.Worldcoin, POOLMANAGER_WORLDCOIN),listener.triggerOnSwapEvent());
+
+
+        // INK
+        addTrigger(chainContract(Chains.Ink, POOLMANAGER_INK),listener.triggerOnInitializeEvent());
+        addTrigger(chainContract(Chains.Ink, POOLMANAGER_INK),listener.triggerOnSwapEvent());
+
+
+        // SONEIUM
+        addTrigger(chainContract(Chains.Soneium, POOLMANAGER_SONEIUM),listener.triggerOnInitializeEvent());
+        addTrigger(chainContract(Chains.Soneium, POOLMANAGER_SONEIUM),listener.triggerOnSwapEvent());
+
+
+        // AVALANCHE
+        addTrigger(chainContract(Chains.Avalanche, POOLMANAGER_AVALANCHE),listener.triggerOnInitializeEvent());
+        addTrigger(chainContract(Chains.Avalanche, POOLMANAGER_AVALANCHE),listener.triggerOnSwapEvent());
+
+
+        // BNB
+        addTrigger(chainContract(Chains.BNB, POOLMANAGER_BNB),listener.triggerOnInitializeEvent());
+        addTrigger(chainContract(Chains.BNB, POOLMANAGER_BNB),listener.triggerOnSwapEvent());
+
+
+        // CELO
+        addTrigger(chainContract(Chains.Celo, POOLMANAGER_CELO),listener.triggerOnInitializeEvent());
+        addTrigger(chainContract(Chains.Celo, POOLMANAGER_CELO),listener.triggerOnSwapEvent());
     }
 }
